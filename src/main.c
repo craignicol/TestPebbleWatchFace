58ed7d2250b0bfb4c0f251aa2660e8607eefcfb9
craignicol <craig.nicol@gmail.com>
2015-01-16 22:41:29 +0000

    Integrate background image.
    http://developer.getpebble.com/getting-started/watchface-tutorial/part2/

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
 
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
@@ -31,10 +34,17 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  s_time_layer = text_layer_create(GRect(2, 50, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
 
   // Improve the layout to be more like a watchface
   // Create GFont
@@ -54,6 +64,12 @@ static void main_window_unload(Window *window) {
 
   // Unload GFont
   fonts_unload_custom_font(s_time_font);  
+    
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);  
 }
 
 static void init() {
